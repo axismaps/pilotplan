f889d271f9529f545b8d441966ff56718cb68eca
David Heyman <dave@axismaps.com>
2018-08-07 00:07:27 +0100

    Automatically download styles file

---
+++ bin/styles.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const path = require('path');
+const mbxStyles = require('@mapbox/mapbox-sdk/services/styles');
+
+const stylesClient = mbxStyles({ accessToken: process.env.MAPBOX_TOKEN });
+
+stylesClient.getStyle({ styleId: process.env.MAPBOX_STYLE })
+  .send()
+  .then((response) => {
+    const style = response.body;
+    fs.writeFile(path.join(__dirname, '../src/data/style.json'), JSON.stringify(style, null, 2), () => {
+      console.log('NEW STYLE WRITTEN');
+    });
+  });
