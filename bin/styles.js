5fe74e9da9e1077aca6eef9712aa6d3c5a72e9ee
David Heyman <dave@axismaps.com>
2018-08-07 08:57:09 +0100

    Called the Mapbox token the wrong thing

--- bin/styles.js
+++ bin/styles.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const mbxStyles = require('@mapbox/mapbox-sdk/services/styles');
 
-const stylesClient = mbxStyles({ accessToken: process.env.MAPBOX_TOKEN });
+const stylesClient = mbxStyles({ accessToken: process.env.MAPBOX_ACCESS_TOKEN });
 
 stylesClient.getStyle({ styleId: process.env.MAPBOX_STYLE })
   .send()
