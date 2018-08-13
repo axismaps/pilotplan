774d3604255cffab22b1eba27a2b9990d3911c13
David Heyman <dave@axismaps.com>
2018-08-13 15:13:38 +0100

    Load features into CSV but it's way too big

--- bin/static.js
+++ bin/static.js
@@ -1,53 +1,92 @@
 const fs = require('fs');
 const path = require('path');
 const _ = require('underscore');
+const stringify = require('csv-stringify');
 
 const config = {};
+const csv = [];
 let loaded = 0;
+let total = 0;
 
 fs.readdir(path.join(__dirname, '../data/geojson/geography'), (err, files) => {
-  files.forEach((f) => {
-    fs.readFile(path.join(__dirname, '../data/geojson/geography', f), (err2, data) => {
-      const name = f.replace(/\.json$/, '');
-      const json = JSON.parse(data);
-      const props = getProps(json);
+  total = files.length;
+  files.forEach(writeStatic);
+});
 
-      const layer = {};
-      layer.features = {};
-      layer.group = name === 'WatersLine' || name === 'WaterBodiesPoly' || name === 'OpenSpacesPoly' ? 'Landscape' : 'Urbanism';
-      layer.icon = json.features[0].geometry.type === 'LineString' ? 'line.svg' : 'poly.svg';
+function writeStatic(f) {
+  fs.readFile(path.join(__dirname, '../data/geojson/geography', f), (err2, data) => {
+    const name = f.replace(/\.json$/, '');
+    const json = JSON.parse(data);
+    const props = getProps(json);
 
-      // Fake translation for now
-      layer.en = name;
-      layer.pr = name;
+    const layer = {};
+    layer.features = {};
+    layer.group = name === 'WatersLine' || name === 'WaterBodiesPoly' || name === 'OpenSpacesPoly' ? 'Landscape' : 'Urbanism';
+    layer.icon = json.features[0].geometry.type === 'LineString' ? 'line.svg' : 'poly.svg';
 
-      props.forEach((p) => {
-        layer.startYear = layer.startYear ? Math.min(layer.startYear, p.FirstYear) : p.FirstYear;
-        layer.endYear = layer.endYear ? Math.max(layer.endYear, p.LastYear) : p.LastYear;
+    // Fake translation for now
+    layer.en = name;
+    layer.pr = name;
 
-        if (p.SubType) {
-          const sub = p.SubType;
-          if (!layer.features[sub]) layer.features[sub] = {};
+    props.forEach((p) => {
+      layer.startYear = layer.startYear ? Math.min(layer.startYear, p.FirstYear) : p.FirstYear;
+      layer.endYear = layer.endYear ? Math.max(layer.endYear, p.LastYear) : p.LastYear;
 
-          // Fake translation for now
-          layer.features[sub].en = sub;
-          layer.features[sub].pr = sub;
+      if (p.SubType) {
+        const sub = p.SubType;
+        if (!layer.features[sub]) layer.features[sub] = {};
 
-          layer.features[sub].startYear = layer.features[sub].startYear ?
-            Math.min(layer.features[sub].startYear, p.FirstYear) : p.FirstYear;
-          layer.features[sub].endYear = layer.features[sub].endYear ?
-            Math.max(layer.features[sub].startYear, p.LastYear) : p.LastYear;
-        }
-      });
+        // Fake translation for now
+        layer.features[sub].en = sub;
+        layer.features[sub].pr = sub;
 
-      config[name] = layer;
-      loaded += 1;
-      if (loaded === files.length) {
-        getStyleInfo();
+        layer.features[sub].startYear = layer.features[sub].startYear ?
+          Math.min(layer.features[sub].startYear, p.FirstYear) : p.FirstYear;
+        layer.features[sub].endYear = layer.features[sub].endYear ?
+          Math.max(layer.features[sub].startYear, p.LastYear) : p.LastYear;
       }
     });
+
+    parseCSV(json);
+    config[name] = layer;
+    loaded += 1;
+    if (loaded === total) {
+      getStyleInfo();
+      writeCSV();
+    }
   });
-});
+}
+
+function parseCSV(json) {
+  json.features.forEach((f) => {
+    if (f.properties.Name && f.geometry) {
+      const row = _.extend(getBounds(f.geometry.coordinates), _.pick(f.properties, ['Name', 'FirstYear', 'LastYear', 'SubType']));
+      csv.push(row);
+    }
+  });
+}
+
+function writeCSV() {
+  stringify(csv, (err, output) => {
+    fs.writeFile(path.join(__dirname, '../src/data/features.csv'), output, () => {
+      console.log('FEATURES WRITTEN');
+    });
+  });
+}
+
+function getBounds(coordinates) {
+  const coords = [];
+  const raw = _.flatten(coordinates);
+  for (let i = 0; i < raw.length; i += 2) {
+    coords.push([raw[i], raw[i + 1]]);
+  }
+  return {
+    xmin: parseFloat(_.reduce(coords, (m, c) => Math.min(m, c[0]), Infinity).toFixed(4)),
+    xmax: parseFloat(_.reduce(coords, (m, c) => Math.max(m, c[0]), -Infinity).toFixed(4)),
+    ymin: parseFloat(_.reduce(coords, (m, c) => Math.min(m, c[1]), Infinity).toFixed(4)),
+    ymax: parseFloat(_.reduce(coords, (m, c) => Math.max(m, c[1]), -Infinity).toFixed(4)),
+  };
+}
 
 function getProps(json) {
   return json.features.map(f => f.properties);
