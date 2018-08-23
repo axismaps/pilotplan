06f9c586b57f61ae686c409f4cd18cef57eba146
David Heyman <dave@axismaps.com>
2018-08-23 09:06:19 +0100

    A way to omit some tiles and then join them together

--- bin/tiles.js
+++ bin/tiles.js
@@ -3,11 +3,19 @@ const fs = require('fs');
 const path = require('path');
 
 fs.readdir(path.join(__dirname, '../data/geojson/geography'), (err, files) => {
-  files.forEach((f) => {
-    const name = f.replace(/\..*/, '').toLowerCase();
-    const c = `tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le1 -x SHAPE_Area -aD -aG -ab -ai -f -o ${path.join(__dirname, `../data/tiles/${name}.mbtiles`)} ${path.join(__dirname, `../data/geojson/geography/${f}`)}`;
+  makeTile(files);
+});
 
-    const tpOut = exec(c);
+function makeTile(files) {
+  const f = files.shift();
+  const name = f.replace(/\..*/, '');
+
+  if (name !== 'ParcelsPoly' && name !== 'BoundariesPoly') {
+    const c = `tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le1 -x SHAPE_Area -Z 9 -aD -aG -ab -ai -f -o ${path.join(__dirname, `../data/tiles/${name}.mbtiles`)} ${path.join(__dirname, `../data/geojson/geography/${f}`)}`;
+
+    const tpOut = exec(c, () => {
+      if (files.length > 0) makeTile(files);
+    });
     tpOut.stdout.on('data', (data) => {
       console.log(data.toString());
     });
@@ -17,16 +25,7 @@ fs.readdir(path.join(__dirname, '../data/geojson/geography'), (err, files) => {
     tpOut.on('exit', (code) => {
       console.log(`child process exited with code ${code.toString()}`);
     });
-
-    const mbOut = exec(`mapbox upload axismaps.pilot-${name} ${path.join(__dirname, `../data/tiles/${name}.mbtiles`)}`);
-    mbOut.stdout.on('data', (data) => {
-      console.log(data.toString());
-    });
-    mbOut.stderr.on('data', (data) => {
-      console.log(data.toString());
-    });
-    mbOut.on('exit', (code) => {
-      console.log(`child process exited with code ${code.toString()}`);
-    });
-  });
-});
+  } else if (files.length > 0) {
+    makeTile(files);
+  }
+}
