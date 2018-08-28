a2597f04128ca62ec354302454971ac82bfb590a
Benjamin Wellington <bwellington@landscapemetrics.com>
2018-08-28 17:33:39 -0400

    Fix highlighted feature search bug

--- src/js/atlasDataMethods.js
+++ src/js/atlasDataMethods.js
@@ -3,8 +3,10 @@ const atlasDataMethods = {
     features.filter(d => Object.prototype.hasOwnProperty.call(d.properties, 'SS_ID') &&
     d.source !== 'viewshed'),
   getNonRasterResults: features =>
-    features.filter(d => !Object.prototype.hasOwnProperty.call(d.properties, 'SS_ID') &&
-Object.prototype.hasOwnProperty.call(d.properties, 'Name')),
+    features.filter(d =>
+      !Object.prototype.hasOwnProperty.call(d.properties, 'SS_ID') &&
+      Object.prototype.hasOwnProperty.call(d.properties, 'Name') &&
+      d.source !== 'highlighted'),
 };
 
 export default atlasDataMethods;
