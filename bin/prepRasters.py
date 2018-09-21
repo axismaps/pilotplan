bc03db95bafc98f12087fb84041982cb0ec241e8
David Heyman <dave@axismaps.com>
2018-09-21 12:16:21 +0100

    Setting no data works as it should

--- bin/prepRasters.py
+++ bin/prepRasters.py
@@ -15,21 +15,19 @@ for subdir in os.listdir(Path):
         f = Path + subdir + tif
         src = gdal.Open(f)
         ras = rasterio.open(f)
-        val = ras.read(1)[0][0]
+        val = min(255, ras.read(1)[0][0])
 
-        if src.RasterCount == 4 or (val != 0 and val != 255 and val != 256):
+        if src.RasterCount == 4 or (val != 0 and val != 255):
           copyfile(f, Path + 'converted/' + tif)
         elif src.RasterCount == 1 or src.RasterCount == 3:
-          s = Template("""gdal_translate -b 1 -ot Byte ${f} red.tif &&
-              gdal_translate -b ${b2} -ot Byte ${f} green.tif && 
-              gdal_translate -b ${b3} -ot Byte ${f} blue.tif &&  
-              gdal_translate -a_nodata ${nodata} ${f} no.tif && 
-              gdal_calc.py -A no.tif --outfile=mask.tif --calc="(A<=255)*255" && 
-              gdal_merge.py -separate -o ${path}converted/${tif} red.tif green.tif blue.tif mask.tif && 
-              rm red.tif && 
-              rm green.tif && 
-              rm blue.tif && 
-              rm no.tif && 
+          s = Template("""gdal_translate -b 1 -ot Byte ${f} -a_nodata none red.tif &&
+              gdal_translate -b ${b2} -ot Byte ${f} -a_nodata none green.tif &&
+              gdal_translate -b ${b3} -ot Byte ${f} -a_nodata none blue.tif && 
+              gdal_calc.py -A red.tif -B green.tif -C blue.tif --outfile=mask.tif --calc="logical_and(A!=${nodata},B!=${nodata},C!=${nodata})*255" --NoDataValue=0 &&
+              gdal_merge.py -separate -o ${path}converted/${tif} red.tif green.tif blue.tif mask.tif &&
+              rm red.tif &&
+              rm green.tif &&
+              rm blue.tif &&
               rm mask.tif""")
           if src.RasterCount == 1:
             os.system(s.substitute(f=f, b2='1', b3='1', nodata=str(val), path=Path, tif=tif))
