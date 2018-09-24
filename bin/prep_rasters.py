3bc4baa75fb0793fa6dacabd180be4f7da56021b
David Heyman <dave@axismaps.com>
2018-09-24 09:26:10 +0100

    Making everything more python-y

--- bin/prep_rasters.py
+++ bin/prep_rasters.py
@@ -1,3 +1,5 @@
+"""Deal with raster transparency and convert to MBTiles for uploading"""
+
 import os
 import re
 from string import Template
@@ -5,23 +7,23 @@ from shutil import copyfile
 from osgeo import gdal
 import rasterio
 
-Path = 'data/geotiff/'
+PATH = 'data/geotiff/'
 
-for subdir in os.listdir(Path):
+for subdir in os.listdir(PATH):
   if subdir[0] != '.' and subdir != 'converted':
     subdir += '/'
-    for tif in os.listdir(Path + subdir):
+    for tif in os.listdir(PATH + subdir):
       if re.search(r"\.tif$", tif):
-        f = Path + subdir + tif
+        f = PATH + subdir + tif
         src = gdal.Open(f)
         ras = rasterio.open(f)
         val = min(255, ras.read(1)[0][0])
 
         if src.RasterCount == 4 or (val != 0 and val != 255):
           if ras.dtypes[0] == 'uint16':
-            os.system('gdal_translate -ot Byte ' + f + ' ' + Path + 'converted/' + tif)
+            os.system('gdal_translate -ot Byte ' + f + ' ' + PATH + 'converted/' + tif)
           else:
-            copyfile(f, Path + 'converted/' + tif)
+            copyfile(f, PATH + 'converted/' + tif)
         elif src.RasterCount == 1 or src.RasterCount == 3:
           s = Template("""gdal_translate -b 1 -ot Byte ${f} -a_nodata none red.tif &&
               gdal_translate -b ${b2} -ot Byte ${f} -a_nodata none green.tif &&
@@ -33,15 +35,15 @@ for subdir in os.listdir(Path):
               rm blue.tif &&
               rm mask.tif""")
           if src.RasterCount == 1:
-            os.system(s.substitute(f=f, b2='1', b3='1', nodata=str(val), path=Path, tif=tif))
+            os.system(s.substitute(f=f, b2='1', b3='1', nodata=str(val), path=PATH, tif=tif))
           elif src.RasterCount == 3:
-            os.system(s.substitute(f=f, b2='2', b3='3', nodata=str(val), path=Path, tif=tif))
+            os.system(s.substitute(f=f, b2='2', b3='3', nodata=str(val), path=PATH, tif=tif))
         else:
           print f + ' has wrong number of bands!'
 
-for tif in os.listdir(Path + 'converted/'):
+for tif in os.listdir(PATH + 'converted/'):
   basename = re.sub(r"\.tif$", '', tif)
   s = Template("""gdalwarp -t_srs EPSG:3857 ${path}converted/${tif} ${path}converted/${base}_merc.tif &&
     gdal_translate -of mbtiles ${path}converted/${base}_merc.tif ${path}converted/${base}.mbtiles &&
     gdaladdo -r nearest ${path}converted/${base}.mbtiles 2 4 8 16 32""")
-  os.system(s.substitute(path=Path, tif=tif, base=basename))
+  os.system(s.substitute(path=PATH, tif=tif, base=basename))
