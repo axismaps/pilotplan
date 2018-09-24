e46f4522b2d3ad1317efdb0e38fbff7f03f87dfc
David Heyman <dave@axismaps.com>
2018-09-24 10:03:16 +0100

    Deal with 4-band uint16 rasters by remapping alpha band

--- bin/prep_rasters.py
+++ bin/prep_rasters.py
@@ -17,26 +17,23 @@ for subdir in os.listdir(PATH):
         f = PATH + subdir + tif
         src = gdal.Open(f)
         ras = rasterio.open(f)
-        val = min(255, ras.read(1)[0][0])
+        val = ras.read(1)[0][0]
 
-        if src.RasterCount == 4 or (val != 0 and val != 255):
-          if ras.dtypes[0] == 'uint16':
-            os.system('gdal_translate -ot Byte ' + f + ' ' + PATH + 'converted/' + tif)
-          else:
-            copyfile(f, PATH + 'converted/' + tif)
-        elif src.RasterCount == 1 or src.RasterCount == 3:
-          s = Template("""gdal_translate -b 1 -ot Byte ${f} -a_nodata none red.tif &&
-              gdal_translate -b ${b2} -ot Byte ${f} -a_nodata none green.tif &&
-              gdal_translate -b ${b3} -ot Byte ${f} -a_nodata none blue.tif && 
-              gdal_calc.py -A red.tif -B green.tif -C blue.tif --outfile=mask.tif --calc="logical_and(A!=${nodata},B!=${nodata},C!=${nodata})*255" --NoDataValue=0 &&
-              gdal_merge.py -separate -o ${path}converted/${tif} red.tif green.tif blue.tif mask.tif &&
+        if (src.RasterCount == 4 or (val != 0 and val != 255)) and ras.dtypes[0] == 'uint8':
+          copyfile(f, PATH + 'converted/' + tif)
+        elif src.RasterCount == 1 or src.RasterCount == 3 or src.RasterCount == 4:
+          s = Template("""gdal_translate -b 1 ${f} -a_nodata none red.tif &&
+              gdal_translate -b ${b2} ${f} -a_nodata none green.tif &&
+              gdal_translate -b ${b3} ${f} -a_nodata none blue.tif && 
+              gdal_calc.py -A red.tif -B green.tif -C blue.tif --outfile=mask.tif --calc="logical_and(A!=${nodata},B!=${nodata},C!=${nodata})*255" &&
+              gdal_merge.py -separate -ot Byte -o ${path}converted/${tif} red.tif green.tif blue.tif mask.tif &&
               rm red.tif &&
               rm green.tif &&
               rm blue.tif &&
               rm mask.tif""")
           if src.RasterCount == 1:
             os.system(s.substitute(f=f, b2='1', b3='1', nodata=str(val), path=PATH, tif=tif))
-          elif src.RasterCount == 3:
+          elif src.RasterCount >= 3:
             os.system(s.substitute(f=f, b2='2', b3='3', nodata=str(val), path=PATH, tif=tif))
         else:
           print f + ' has wrong number of bands!'
