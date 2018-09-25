2c963c31f8764d069bd2c15cd107bdaf3037db7b
David Heyman <dave@axismaps.com>
2018-09-25 13:44:54 +0100

    Buildings don't start until 12

--- bin/tiles.sh
+++ bin/tiles.sh
@@ -9,7 +9,7 @@ tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x 
   data/geojson/geography/WaterBodiesPoly.json \
   data/geojson/geography/WatersLine.json
 
-tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le1 -x SHAPE_Area -Z 11 -aD -aG -ab -ai -f -o data/tiles/BuildingsPoly.mbtiles data/geojson/geography/BuildingsPoly.json
+tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le1 -x SHAPE_Area -Z 12 -aD -aG -ab -ai -f -o data/tiles/BuildingsPoly.mbtiles data/geojson/geography/BuildingsPoly.json
 
 tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le1 -x SHAPE_Area -Z 14 -aD -aG -ab -ai -f -o data/tiles/ParcelsPoly.mbtiles data/geojson/geography/ParcelsPoly.json
 
