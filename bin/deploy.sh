b5f1670620e0c4056a699564dae0394abe3a772c
David Heyman <dave@axismaps.com>
2018-10-04 08:05:56 +0100

    Delete old files when deploying

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,3 +1,3 @@
 npm run build
-aws s3 sync dist/ s3://pilotplan.axismaps.com
+aws s3 sync dist/ s3://pilotplan.axismaps.com --delete
 # aws cloudfront create-invalidation --distribution-id EXJ4VM8AU5FRR --paths /\*
