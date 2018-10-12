ff6e2951bafc0311a787330025137ddca5bcac1f
Benjamin Wellington <bwellington@landscapemetrics.com>
2018-10-12 10:32:09 -0400

    Further refine slider axis, per #49

--- src/js/timelineSliderAxis.js
+++ src/js/timelineSliderAxis.js
@@ -38,12 +38,9 @@ const axisMethods = ({ privateProps, privateMethods }) => ({
       scale,
       stepSections,
     } = props;
-    const domain = scale.domain();
-    console.log('domain', domain);
-    console.log('stepSections', stepSections);
-    // const tickValues = [stepSections[0].years[0], 1900, 1910, 1920, 1930, 1940, 1950];
+    const { isLabeledTick } = privateMethods;
 
-    const tickValues = [stepSections[0].years[0]];
+    const tickValues = [];
     const span1 = stepSections[0];
     const span2 = stepSections[1];
     const increment1 = 20;
@@ -51,37 +48,31 @@ const axisMethods = ({ privateProps, privateMethods }) => ({
     const numTicks1 = Math.round((span1.years[1] - span1.years[0]) / increment1);
     const numTicks2 = Math.round((span2.years[1] - span2.years[0]) / increment2);
 
-    // const addTicks = (span, tickValue, round) => {
-    //   const { increment, years } = span;
-    //   // const tickValue = oneTickValue * increment;
-    //   const numTicks = Math.round((years[1] - years[0]) / tickValue);
-    //   const startPosition = tickValues.length - 1;
-    //   for (let i = 0;
-    //     i < numTicks;
-    //     i += 1) {
-    //     tickValues.push(Math.round((tickValues[startPosition + i] + tickValue) / round) * round);
-    //   }
-    // };
-
     for (let i = 0;
       i < numTicks1;
       i += 1) {
-      tickValues.push(Math.round((tickValues[i] + increment1) / 10) * 10);
+      tickValues.push(Math.round((stepSections[0].years[0] + (increment1 * (i + 1))) / 10) * 10);
     }
+    const startValSeg2 = tickValues.slice(-1)[0];
 
     for (let i = 1;
       i < numTicks2;
       i += 1
     ) {
-      tickValues.push(Math.round(tickValues[numTicks1] + ((i + 1) * increment2)));
+      tickValues.push(Math.round(startValSeg2 + ((i + 1) * increment2)));
     }
-    console.log('tickValues', tickValues);
 
     props.axis = d3.axisBottom(scale)
       // .ticks(20)
       // .ticks([1900, 1920, 1950, 1975])
       .tickValues(tickValues)
-      .tickFormat(d => (d % 10 === 0 ? d : ''));
+      .tickFormat(d => (isLabeledTick({ value: d, breakpoint: stepSections[0].years[1] }) ? d : ''));
+  },
+  isLabeledTick({ value, breakpoint }) {
+    if (value > breakpoint) {
+      return value % 10 === 0;
+    }
+    return [1930, 1892].includes(value);
   },
   updateAxis() {
     const props = privateProps.get(this);
@@ -89,11 +80,12 @@ const axisMethods = ({ privateProps, privateMethods }) => ({
       axisGroup,
       axis,
       axisOn,
+      stepSections,
     } = props;
+    const { isLabeledTick } = privateMethods;
+
     if (!axisOn) return;
 
-    // console.log('axisgroup', axisGroup);
-    // console.log('axis', axis);
 
     axisGroup
       .call(axis);
@@ -102,10 +94,9 @@ const axisMethods = ({ privateProps, privateMethods }) => ({
       .selectAll('.tick')
       .each(function setMinorTick(d) {
         const tick = d3.select(this).select('line');
-        if (d % 10 !== 0) {
+        if (!isLabeledTick({ value: d, breakpoint: stepSections[0].years[1] })) {
           tick.attr('y2', 10);
         }
-        // console.log('d', d);
       });
   },
 });
