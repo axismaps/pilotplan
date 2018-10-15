d765250c3c97b6d5d1e6b562ba1992220c558263
David Heyman <dave@axismaps.com>
2018-10-15 08:04:26 +0100

    Updated tile generation and styles for #48

--- bin/tiles.sh
+++ bin/tiles.sh
@@ -1,4 +1,5 @@
 tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le1 -x SHAPE_Area -Z 9 -z 17 -aD -aG -ab -ai -f -o data/tiles/allzooms.mbtiles \
+  -j '{ "RoadsLine": ["!in", "SubType", "Collector", "Local", "Arterial"] }' \
   data/geojson/geography/BoundariesPoly.json \
   data/geojson/geography/BuiltDomainPoly.json \
   data/geojson/geography/InfrastructureLine.json \
@@ -9,11 +10,19 @@ tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x 
   data/geojson/geography/WaterBodiesPoly.json \
   data/geojson/geography/WatersLine.json
 
+tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le1 -x SHAPE_Area -Z 11 -z 17 -aD -aG -ab -ai -f -o data/tiles/ArterialRoads.mbtiles \
+  -j '{ "*": ["==", "SubType", "Arterial"] }' \
+  data/geojson/geography/RoadsLine.json
+
+tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le1 -x SHAPE_Area -Z 12 -z 17 -aD -aG -ab -ai -f -o data/tiles/LocalRoads.mbtiles \
+  -j '{ "*": ["in", "SubType", "Collector", "Local"] }' \
+  data/geojson/geography/RoadsLine.json
+
 tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le1 -x SHAPE_Area -Z 12 -z 17 -aD -aG -ab -ai -f -o data/tiles/BuildingsPoly.mbtiles data/geojson/geography/BuildingsPoly.json
 
 tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le1 -x SHAPE_Area -Z 14 -z 17 -aD -aG -ab -ai -f -o data/tiles/ParcelsPoly.mbtiles data/geojson/geography/ParcelsPoly.json
 
-tile-join -f -o data/tiles/pilot.mbtiles data/tiles/allzooms.mbtiles data/tiles/BuildingsPoly.mbtiles data/tiles/ParcelsPoly.mbtiles
+tile-join -f -o data/tiles/pilot.mbtiles data/tiles/allzooms.mbtiles data/tiles/BuildingsPoly.mbtiles data/tiles/ParcelsPoly.mbtiles data/tiles/ArterialRoads.mbtiles data/tiles/LocalRoads.mbtiles
 
 source .env
 
