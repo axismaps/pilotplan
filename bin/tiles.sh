c17a11ed4b68af529da278af3d8ecb16fce9a999
David Heyman <dave@axismaps.com>
2019-01-08 12:21:06 +0000

    The better way to create multiple zoom params

--- bin/tiles.sh
+++ bin/tiles.sh
@@ -1,5 +1,7 @@
-tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title -Z 9 -z 17 -aD -aG -ab -ai -f -o data/tiles/allzooms.mbtiles \
-  -j '{ "RoadsLine": ["!in", "SubType", "Collector", "Local", "Arterial", "Service"] }' \
+# No local roads buildings, or parcels
+tippecanoe -Z 9 -z 11 -aD -aG -ab -ai -f -o data/tiles/allzooms.mbtiles \
+  -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title \
+  -j '{ "RoadsLine": ["!in", "SubType", "Collector", "Local", "Service"] }' \
   data/geojson/geography/BoundariesPoly.json \
   data/geojson/geography/BuiltDomainPoly.json \
   data/geojson/geography/InfrastructureLine.json \
@@ -10,19 +12,51 @@ tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x 
   data/geojson/geography/WaterBodiesPoly.json \
   data/geojson/geography/WatersLine.json
 
-tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title -Z 11 -z 17 -aD -aG -ab -ai -f -o data/tiles/ArterialRoads.mbtiles \
-  -j '{ "*": ["==", "SubType", "Arterial"] }' \
-  data/geojson/geography/RoadsLine.json
+# Adding in buildings
+tippecanoe -Z 12 -z 12 -aD -aG -ab -ai -f -o data/tiles/BuildingsPoly.mbtiles \
+  -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title \
+  -j '{ "RoadsLine": ["!in", "SubType", "Collector", "Local", "Service"] }' \
+  data/geojson/geography/BoundariesPoly.json \
+  data/geojson/geography/BuildingsPoly.json \
+  data/geojson/geography/BuiltDomainPoly.json \
+  data/geojson/geography/InfrastructureLine.json \
+  data/geojson/geography/OpenSpacesPoly.json \
+  data/geojson/geography/RoadsLine.json \
+  data/geojson/geography/SectorsPoly.json \
+  data/geojson/geography/ViewConesPoint.json \
+  data/geojson/geography/WaterBodiesPoly.json \
+  data/geojson/geography/WatersLine.json
 
-tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title -Z 13 -z 17 -aD -aG -ab -ai -f -o data/tiles/LocalRoads.mbtiles \
-  -j '{ "*": ["in", "SubType", "Collector", "Local", "Service"] }' \
-  data/geojson/geography/RoadsLine.json
+# Removing filter to add local roads
+tippecanoe -Z 13 -z 13 -aD -aG -ab -ai -f -o data/tiles/LocalRoads.mbtiles \
+  -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title \
+  data/geojson/geography/BoundariesPoly.json \
+  data/geojson/geography/BuildingsPoly.json \
+  data/geojson/geography/BuiltDomainPoly.json \
+  data/geojson/geography/InfrastructureLine.json \
+  data/geojson/geography/OpenSpacesPoly.json \
+  data/geojson/geography/RoadsLine.json \
+  data/geojson/geography/SectorsPoly.json \
+  data/geojson/geography/ViewConesPoint.json \
+  data/geojson/geography/WaterBodiesPoly.json \
+  data/geojson/geography/WatersLine.json
 
-tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title -Z 12 -z 17 -aD -aG -ab -ai -f -o data/tiles/BuildingsPoly.mbtiles data/geojson/geography/BuildingsPoly.json
+# Adding parcels and doing all zooms
+tippecanoe -Z 14 -z 17 -aD -aG -ab -ai -f -o data/tiles/ParcelsPoly.mbtiles \
+  -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title \
+  data/geojson/geography/BoundariesPoly.json \
+  data/geojson/geography/BuildingsPoly.json \
+  data/geojson/geography/BuiltDomainPoly.json \
+  data/geojson/geography/InfrastructureLine.json \
+  data/geojson/geography/OpenSpacesPoly.json \
+  data/geojson/geography/ParcelsPoly.json \
+  data/geojson/geography/RoadsLine.json \
+  data/geojson/geography/SectorsPoly.json \
+  data/geojson/geography/ViewConesPoint.json \
+  data/geojson/geography/WaterBodiesPoly.json \
+  data/geojson/geography/WatersLine.json
 
-tippecanoe -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title -Z 14 -z 17 -aD -aG -ab -ai -f -o data/tiles/ParcelsPoly.mbtiles data/geojson/geography/ParcelsPoly.json
-
-tile-join -f -o data/tiles/pilot.mbtiles data/tiles/allzooms.mbtiles data/tiles/BuildingsPoly.mbtiles data/tiles/ParcelsPoly.mbtiles data/tiles/ArterialRoads.mbtiles data/tiles/LocalRoads.mbtiles
+tile-join -f -o data/tiles/pilot.mbtiles data/tiles/allzooms.mbtiles data/tiles/BuildingsPoly.mbtiles data/tiles/ParcelsPoly.mbtiles data/tiles/LocalRoads.mbtiles
 
 source .env
 
