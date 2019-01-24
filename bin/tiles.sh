f80ede189a3693f30aeebcc62dc518f1a8f9762e
David Heyman <dave@axismaps.com>
2019-01-24 12:18:37 +0000

    Updates to tile script

--- bin/tiles.sh
+++ bin/tiles.sh
@@ -1,68 +1,74 @@
-# No local roads buildings, or parcels
-tippecanoe -Z 9 -z 11 -aN -ab -ai -f -o data/tiles/allzooms.mbtiles \
+tippecanoe -Z 9 -z 10 -S 10 -ab -ai -pf -pk -f -o data/tiles/10.mbtiles \
   -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title \
-  -j '{ "RoadsLine": ["!in", "SubType", "Collector", "Local", "Service"] }' \
+  -j '{ "RoadsLine": ["!in", "Type", "Tertiary", "Unclassified", "Primary_link", "Secondary_link", "Tertiary_link", "Residential", "Service", "Street", "Pedestrian", "Track", "Track_grade5", "Path", "Cycleway", "Footway"] }' \
+  -j '{ "HidrographyPoly" : ["!in", "Type", "Flood", ""] }' \
   data/geojson/geography/BoundariesPoly.json \
-  data/geojson/geography/BuiltDomainPoly.json \
-  data/geojson/geography/InfrastructureLine.json \
+  data/geojson/geography/GroundCoverPoly.json \
+  data/geojson/geography/HidrographyLine.json \
+  data/geojson/geography/HidrographyPoly.json \
+  data/geojson/geography/RoadsLine.json \
+  data/geojson/geography/UtilitiesLine.json \
+  data/geojson/geography/UtilitiesPoly.json
+
+tippecanoe -Z 11 -z 11 -S 10 -ab -ai -pf -pk -f -o data/tiles/11.mbtiles \
+  -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title \
+  -j '{ "RoadsLine": ["!in", "Type", "Residential", "Service", "Street", "Pedestrian", "Track", "Track_grade5", "Path", "Cycleway", "Footway"] }' \
+  -j '{ "HidrographyPoly" : ["!in", "Type", "Flood", ""] }' \
+  data/geojson/geography/BoundariesPoly.json \
+  data/geojson/geography/GroundCoverPoly.json \
+  data/geojson/geography/HidrographyLine.json \
+  data/geojson/geography/HidrographyPoly.json \
+  data/geojson/geography/RoadsLine.json \
+  data/geojson/geography/UtilitiesLine.json \
+  data/geojson/geography/UtilitiesPoly.json
+
+tippecanoe -Z 12 -z 12 -S 10 -ab -ai -pf -pk -f -o data/tiles/12.mbtiles \
+  -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title \
+  -j '{ "RoadsLine": ["!in", "Type", "Street", "Pedestrian", "Track", "Track_grade5", "Path", "Cycleway", "Footway"] }' \
+  -j '{ "OpenSpacesPoly": ["in", "Type", "Square", "park"] }' \
+  -j '{ "HidrographyPoly" : ["!in", "Type", "Flood", ""] }' \
+  data/geojson/geography/BoundariesPoly.json \
+  data/geojson/geography/GroundCoverPoly.json \
+  data/geojson/geography/HidrographyLine.json \
+  data/geojson/geography/HidrographyPoly.json \
   data/geojson/geography/OpenSpacesPoly.json \
   data/geojson/geography/RoadsLine.json \
-  data/geojson/geography/SectorsPoly.json \
-  data/geojson/geography/ViewConesPoint.json \
-  data/geojson/geography/WaterBodiesPoly.json \
-  data/geojson/geography/WatersLine.json
+  data/geojson/geography/UtilitiesLine.json \
+  data/geojson/geography/UtilitiesPoly.json
 
-# Adding in buildings
-tippecanoe -Z 12 -z 12 -aN -ab -ai -f -o data/tiles/BuildingsPoly.mbtiles \
+tippecanoe -Z 13 -z 13 -S 10 -ab -ai -pf -pk -f -o data/tiles/13.mbtiles \
   -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title \
-  -j '{ "RoadsLine": ["!in", "SubType", "Collector", "Local", "Service"] }' \
+  -j '{ "OpenSpacesPoly": ["in", "Type", "Square", "park"] }' \
+  -j '{ "HidrographyPoly" : ["!in", "Type", "Flood", ""] }' \
+  data/geojson/geography/BoundariesPoly.json \
+  data/geojson/geography/GroundCoverPoly.json \
+  data/geojson/geography/HidrographyLine.json \
+  data/geojson/geography/HidrographyPoly.json \
+  data/geojson/geography/OpenSpacesPoly.json \
+  data/geojson/geography/RoadsLine.json \
+  data/geojson/geography/UtilitiesLine.json \
+  data/geojson/geography/UtilitiesPoly.json
+
+tippecanoe -Z 14 -z 17 -S 10 -ab -ai -pf -pk -f -o data/tiles/14.mbtiles \
+  -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title \
+  -j '{ "HidrographyPoly" : ["!in", "Type", "Flood", ""] }' \
   data/geojson/geography/BoundariesPoly.json \
   data/geojson/geography/BuildingsPoly.json \
-  data/geojson/geography/BuiltDomainPoly.json \
-  data/geojson/geography/InfrastructureLine.json \
+  data/geojson/geography/GroundCoverPoly.json \
+  data/geojson/geography/HidrographyLine.json \
+  data/geojson/geography/HidrographyPoly.json \
   data/geojson/geography/OpenSpacesPoly.json \
   data/geojson/geography/RoadsLine.json \
-  data/geojson/geography/SectorsPoly.json \
-  data/geojson/geography/ViewConesPoint.json \
-  data/geojson/geography/WaterBodiesPoly.json \
-  data/geojson/geography/WatersLine.json
+  data/geojson/geography/UtilitiesLine.json \
+  data/geojson/geography/UtilitiesPoly.json
 
-# Removing filter to add local roads
-tippecanoe -Z 13 -z 13 -aN -ab -ai -f -o data/tiles/LocalRoads.mbtiles \
-  -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title \
-  data/geojson/geography/BoundariesPoly.json \
-  data/geojson/geography/BuildingsPoly.json \
-  data/geojson/geography/BuiltDomainPoly.json \
-  data/geojson/geography/InfrastructureLine.json \
-  data/geojson/geography/OpenSpacesPoly.json \
-  data/geojson/geography/RoadsLine.json \
-  data/geojson/geography/SectorsPoly.json \
-  data/geojson/geography/ViewConesPoint.json \
-  data/geojson/geography/WaterBodiesPoly.json \
-  data/geojson/geography/WatersLine.json
-
-# Adding parcels and doing all zooms
-tippecanoe -Z 14 -z 17 -aN -ab -ai -f -o data/tiles/ParcelsPoly.mbtiles \
-  -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title \
-  data/geojson/geography/BoundariesPoly.json \
-  data/geojson/geography/BuildingsPoly.json \
-  data/geojson/geography/BuiltDomainPoly.json \
-  data/geojson/geography/InfrastructureLine.json \
-  data/geojson/geography/OpenSpacesPoly.json \
-  data/geojson/geography/ParcelsPoly.json \
-  data/geojson/geography/RoadsLine.json \
-  data/geojson/geography/SectorsPoly.json \
-  data/geojson/geography/ViewConesPoint.json \
-  data/geojson/geography/WaterBodiesPoly.json \
-  data/geojson/geography/WatersLine.json
-
-tile-join -f -o data/tiles/pilot.mbtiles data/tiles/allzooms.mbtiles data/tiles/BuildingsPoly.mbtiles data/tiles/ParcelsPoly.mbtiles data/tiles/LocalRoads.mbtiles
+tile-join -pk -f -o data/tiles/houston.mbtiles data/tiles/10.mbtiles data/tiles/11.mbtiles data/tiles/12.mbtiles data/tiles/13.mbtiles data/tiles/14.mbtiles
 
 if [ "$1" == dev ]; then
-  id="axismaps.01c7rsvl"
+  id="axismaps.4dn6opyb"
   tileset="dev"
 else
-  id="axismaps.dd66zwg7"
+  id="axismaps.4dn6opyb"
   tileset="production"
 fi
 
@@ -70,4 +76,4 @@ echo Uploading to $tileset tileset
 
 source .env
 
-mapbox upload $id data/tiles/pilot.mbtiles
+mapbox upload $id data/tiles/houston.mbtiles
