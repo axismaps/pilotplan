639036ed5a17c6f2428ccbfd879943ae57238cee
David Heyman <dave@axismaps.com>
2019-01-27 14:22:33 +0000

    Fixes #65

--- src/js/atlas/atlasMethods.js
+++ src/js/atlas/atlasMethods.js
@@ -110,6 +110,7 @@ const atlasMethods = {
     const mbMap = new mapboxgl.Map({
       minZoom: 9,
       maxZoom: 17,
+      maxBounds: [[-49.6726203, -17.0908045], [-46.4447028, -12.9291031]],
       logoPosition: 'bottom-right',
       container: 'map',
       style,
