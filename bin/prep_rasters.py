37c278538d1c50b754cc88f58eb7c4aac28f8081
David Heyman <dave@axismaps.com>
2019-02-04 11:55:20 +0000

    Code cleanup

--- bin/prep_rasters.py
+++ bin/prep_rasters.py
@@ -12,36 +12,41 @@ PATH = 'data/geotiff/'
 
 def raster_bands(tif, sub):
   if re.search(r"\.tif$", tif):
-    f = PATH + sub + tif
-    print 'Reading raster', f
-    src = gdal.Open(f)
-    ras = rasterio.open(f)
+    tif_file = PATH + sub + tif
+    print 'Reading raster', tif_file
+    src = gdal.Open(tif_file)
+    ras = rasterio.open(tif_file)
     val = ras.read(1)[0][0]
 
     if (src.RasterCount == 4 or (val != 0 and val != 255)) and ras.dtypes[0] == 'uint8':
       print '4 correct bands found'
-      copyfile(f, PATH + 'converted/' + tif)
+      copyfile(tif_file, PATH + 'converted/' + tif)
       project_raster(tif)
     elif src.RasterCount == 1 or src.RasterCount == 3 or src.RasterCount == 4:
-      s = Template("""gdal_translate -b 1 ${f} -a_nodata none red.tif &&
+      gdal_string = Template("""gdal_translate -b 1 ${f} -a_nodata none red.tif &&
           gdal_translate -b ${b2} ${f} -a_nodata none green.tif &&
-          gdal_translate -b ${b3} ${f} -a_nodata none blue.tif && 
+          gdal_translate -b ${b3} ${f} -a_nodata none blue.tif &&
+          echo Calculating file mask
           gdal_calc.py -A red.tif -B green.tif -C blue.tif --outfile=mask.tif --calc="logical_and(A!=${nodata},B!=${nodata},C!=${nodata})*255" --NoDataValue=0 &&
+          echo Merging files
           gdal_merge.py -separate -ot Byte -o ${path}converted/${tif} red.tif green.tif blue.tif mask.tif &&
+          echo Cleaning up
           rm red.tif &&
           rm green.tif &&
           rm blue.tif &&
           rm mask.tif""")
       if src.RasterCount == 1:
         print '1 band raster found'
-        os.system(s.substitute(f=f, b2='1', b3='1', nodata=str(val), path=PATH, tif=tif))
+        os.system(gdal_string.substitute(f=tif_file, b2='1', b3='1', nodata=str(val),
+                                         path=PATH, tif=tif))
         project_raster(tif)
       elif src.RasterCount >= 3:
         print '3+ band raster found'
-        os.system(s.substitute(f=f, b2='2', b3='3', nodata=str(val), path=PATH, tif=tif))
+        os.system(gdal_string.substitute(f=tif_file, b2='2', b3='3', nodata=str(val),
+                                         path=PATH, tif=tif))
         project_raster(tif)
     else:
-      print f + ' has wrong number of bands!'
+      print tif_file + ' has wrong number of bands!'
 
 def project_raster(tif):
   basename = re.sub(r"\.tif$", '', tif)
