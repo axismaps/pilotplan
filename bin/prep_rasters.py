d1d9fd66636fe16c7f8cda992a7de8ef4fe5fabc
David Heyman <dave@axismaps.com>
2019-02-05 10:08:15 +0000

    Interim commit before building argparser

--- bin/prep_rasters.py
+++ bin/prep_rasters.py
@@ -23,18 +23,19 @@ def raster_bands(tif, sub):
       copyfile(tif_file, PATH + 'converted/' + tif)
       project_raster(tif)
     elif src.RasterCount == 1 or src.RasterCount == 3 or src.RasterCount == 4:
-      gdal_string = Template("""gdal_translate -b 1 ${f} -a_nodata none red.tif &&
-          gdal_translate -b ${b2} ${f} -a_nodata none green.tif &&
-          gdal_translate -b ${b3} ${f} -a_nodata none blue.tif &&
+      print 'Using ' + str(val) + ' for no data'
+      gdal_string = Template("""gdal_translate -b 1 ${f} -a_nodata none ${path}converted/red.tif &&
+          gdal_translate -b ${b2} ${f} -a_nodata none ${path}converted/green.tif &&
+          gdal_translate -b ${b3} ${f} -a_nodata none ${path}converted/blue.tif &&
           echo Calculating file mask
-          gdal_calc.py -A red.tif -B green.tif -C blue.tif --outfile=mask.tif --calc="logical_and(A!=${nodata},B!=${nodata},C!=${nodata})*255" --NoDataValue=0 &&
+          gdal_calc.py -A ${path}converted/red.tif -B ${path}converted/green.tif -C ${path}converted/blue.tif --outfile=${path}converted/mask.tif --calc="logical_and(A!=${nodata},B!=${nodata},C!=${nodata})*255" --NoDataValue=0 &&
           echo Merging files
-          gdal_merge.py -separate -ot Byte -o ${path}converted/${tif} red.tif green.tif blue.tif mask.tif &&
+          gdal_merge.py -separate -ot Byte -o ${path}converted/${tif} ${path}converted/red.tif ${path}converted/green.tif ${path}converted/blue.tif ${path}converted/mask.tif &&
           echo Cleaning up
-          rm red.tif &&
-          rm green.tif &&
-          rm blue.tif &&
-          rm mask.tif""")
+          rm ${path}converted/red.tif &&
+          rm ${path}converted/green.tif &&
+          rm ${path}converted/blue.tif &&
+          rm ${path}converted/mask.tif""")
       if src.RasterCount == 1:
         print '1 band raster found'
         os.system(gdal_string.substitute(f=tif_file, b2='1', b3='1', nodata=str(val),
@@ -50,18 +51,25 @@ def raster_bands(tif, sub):
 
 def project_raster(tif):
   basename = re.sub(r"\.tif$", '', tif)
-  mb_string = Template("""gdalwarp -t_srs EPSG:3857 \
-      ${path}converted/${tif} ${path}converted/${base}_merc.tif &&
-    echo Converting to MBTiles
-    gdal2mbtiles --no-fill-borders --min-resolution 9 \
-      ${path}converted/${base}_merc.tif ${path}converted/${base}.mbtiles""")
+  merc_string = Template("""gdalwarp -t_srs EPSG:3857 \
+    ${path}converted/${tif} ${path}converted/${base}_merc.tif""")
+  os.system(merc_string.substitute(path=PATH, tif=tif, base=basename))
+
+  if len(sys.argv) > 2 and sys.argv[2] == '--fixed':
+    mb_string = Template("""echo Converting to MBTiles &&
+      gdal2mbtiles --no-fill-borders --min-resolution 9 --max-resolution 17\
+        ${path}converted/${base}_merc.tif ${path}converted/${base}.mbtiles""")
+  else:
+    mb_string = Template("""gdal_translate -of mbtiles \
+        ${path}converted/${base}_merc.tif ${path}converted/${base}.mbtiles &&
+      gdaladdo -r nearest ${path}converted/${base}.mbtiles 2 4 8 16 32 64""")
   os.system(mb_string.substitute(path=PATH, tif=tif, base=basename))
 
 if sys.argv[1]:
   FILES = sys.argv[1].replace(PATH, '').split('/')
   raster_bands(FILES[1], FILES[0] + '/')
   os.system('source .env && node bin/raster.js')
-  os.system('find data/geotiff/converted/ -type f  ! -name ".gitignore"  -delete')
+  # os.system('find data/geotiff/converted/ -type f  ! -name ".gitignore"  -delete')
 else:
   for subdir in os.listdir(PATH):
     if subdir[0] != '.' and subdir != 'converted':
