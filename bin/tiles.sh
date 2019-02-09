93a0f38778448ee89bc76c7abbda578da89cd405
David Heyman <dave@axismaps.com>
2019-02-09 21:37:20 +0000

    Run views before generating tiles

--- bin/tiles.sh
+++ bin/tiles.sh
@@ -1,3 +1,5 @@
+npm run views
+
 # No local roads buildings, or parcels
 tippecanoe -Z 9 -z 11 -ab -ai -f -o data/tiles/allzooms.mbtiles \
   -x osm_id -x Shape_Leng -x Shape_Area -x layer -x Date -x Address -x SHAPE_Le_1 -x Shape_Leng -x SHAPE_Area -x Notes -x Join_Count -x TARGET_FID -x OBJECTID -x Height -x alturaapro -x ano -x Title \
@@ -8,7 +10,6 @@ tippecanoe -Z 9 -z 11 -ab -ai -f -o data/tiles/allzooms.mbtiles \
   data/geojson/geography/OpenSpacesPoly.json \
   data/geojson/geography/RoadsLine.json \
   data/geojson/geography/SectorsPoly.json \
-  data/geojson/geography/ViewConesPoint.json \
   data/geojson/geography/WaterBodiesPoly.json \
   data/geojson/geography/WatersLine.json
 
@@ -23,7 +24,6 @@ tippecanoe -Z 12 -z 12 -aN -ab -ai -f -o data/tiles/BuildingsPoly.mbtiles \
   data/geojson/geography/OpenSpacesPoly.json \
   data/geojson/geography/RoadsLine.json \
   data/geojson/geography/SectorsPoly.json \
-  data/geojson/geography/ViewConesPoint.json \
   data/geojson/geography/WaterBodiesPoly.json \
   data/geojson/geography/WatersLine.json
 
@@ -37,7 +37,6 @@ tippecanoe -Z 13 -z 13 -aN -ab -ai -f -o data/tiles/LocalRoads.mbtiles \
   data/geojson/geography/OpenSpacesPoly.json \
   data/geojson/geography/RoadsLine.json \
   data/geojson/geography/SectorsPoly.json \
-  data/geojson/geography/ViewConesPoint.json \
   data/geojson/geography/WaterBodiesPoly.json \
   data/geojson/geography/WatersLine.json
 
@@ -52,11 +51,12 @@ tippecanoe -Z 14 -z 17 -aN -ab -ai -f -o data/tiles/ParcelsPoly.mbtiles \
   data/geojson/geography/ParcelsPoly.json \
   data/geojson/geography/RoadsLine.json \
   data/geojson/geography/SectorsPoly.json \
-  data/geojson/geography/ViewConesPoint.json \
   data/geojson/geography/WaterBodiesPoly.json \
   data/geojson/geography/WatersLine.json
 
-tile-join -f -o data/tiles/pilot.mbtiles data/tiles/allzooms.mbtiles data/tiles/BuildingsPoly.mbtiles data/tiles/ParcelsPoly.mbtiles data/tiles/LocalRoads.mbtiles
+tippecanoe -Z 9 -z 17 -r1 -pk -pf -f -o data/tiles/ViewCones.mbtiles data/geojson/geography/ViewConesPoint.json
+
+tile-join -f -o data/tiles/pilot.mbtiles data/tiles/allzooms.mbtiles data/tiles/BuildingsPoly.mbtiles data/tiles/ParcelsPoly.mbtiles data/tiles/LocalRoads.mbtiles data/tiles/ViewCones.mbtiles
 
 if [ "$1" == dev ]; then
   id="axismaps.01c7rsvl"
