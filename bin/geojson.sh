f65278d9652c926e4d20cef690c1a18208881311
David Heyman <dave@axismaps.com>
2019-05-12 18:49:27 -0500

    Geojson conversion with simplification

--- bin/geojson.sh
+++ bin/geojson.sh
@@ -1,11 +1,28 @@
 for f in $1*.shp;\
-  do mapshaper $f field-types=FirstYear:number,LastYear:number -o data/geojson/$(basename "$f" .shp).json;\
+  do mapshaper $f field-types=FirstYear:number,LastYear:number \
+    -each 'Name=null' where='Name=undefined' \
+    -each 'Type=null' where='Type=undefined' \
+    -each 'FirstYear=Math.floor(FirstDate / 10000)' where='!FirstYear' \
+    -each 'LastYear=Math.floor(LastDate / 10000)' where='!LastYear' \
+    -each 'FirstDate=null' where='FirstDate=undefined' \
+    -each 'LastDate=null' where='LastDate=undefined' \
+    -filter-fields FirstYear,LastYear,FirstDate,LastDate,Name,Type \
+    -o data/geojson/$(basename "$f" .shp).json;\
 done
 
 mv data/geojson/AerialExtentsPoly.json data/geojson/visual/AerialExtentsPoly.json
 mv data/geojson/BasemapExtentsPoly.json data/geojson/visual/BasemapExtentsPoly.json
 mv data/geojson/MapExtentsPoly.json data/geojson/visual/MapExtentsPoly.json
 mv data/geojson/PlanExtentsPoly.json data/geojson/visual/PlanExtentsPoly.json
-mv data/geojson/ViewConesPoly.json data/geojson/visual/ViewConesPoly.json
+mv data/geojson/ViewConeExtentsPoly.json data/geojson/visual/ViewConeExtentsPoly.json
 
-mv data/geojson/*.json data/geojson/geography/
+mv data/geojson/*.json data/geojson/geography/high/
+
+for f in data/geojson/geography/high/*;\
+  do mapshaper $f \
+    -simplify 0.05 keep-shapes no-repair \
+    -o data/geojson/geography/low/$(basename "$f");\
+  mapshaper $f \
+    -simplify 0.05 keep-shapes no-repair \
+    -o data/geojson/geography/med/$(basename "$f");\
+done
